9d54e54b0be3dec323e13e8d0b7e598b2f3a5896
cedric-winbush <cawinbushjr@gmail.com>
2020-03-10 14:33:42 -0500

    add initial routes for /api/posts

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -1,8 +1,46 @@
 const express = require('express');
 
-const Posts = require('./posts-model.js');
+const Posts = require('../data/db.js');
 
 const router = express.Router();
 
 
 module.exports = router;
+//the router handles endpoints that begin with /api/posts
+
+router.get('/', (req, res) => {
+  Posts.find(req.query)
+  .then(posts => {
+    res.status(200).json(posts);
+  })
+  .catch(error => {
+    console.log(error);
+    res.status(500).json({
+      message: 'Error retrieving the posts',
+    });
+  });
+});
+
+router.get('/:id', (req , res) => {
+
+});
+
+router.get('/:id/comments', (req, res) => {
+
+});
+
+router.post('/', (req, res) => {
+
+});
+
+router.post('/:id/comments', (req, res) => {
+
+});
+
+router.delete('/:id', (req, res) => {
+
+});
+
+router.put('/:id', (req, res) => {
+  
+})
