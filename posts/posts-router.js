4bd97ad1d29d56778533b1f19c0cc4cc851604cf
cedric-winbush <cawinbushjr@gmail.com>
2020-03-10 15:49:20 -0500

    add get and post router calls without all error handling

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -22,7 +22,14 @@ router.get('/', (req, res) => {
 });
 
 router.get('/:id', (req , res) => {
-
+  Posts.findById(req.params.id)
+  .then(post => {
+    if (post) {
+      res.status(200).json(post);
+    } else{
+      res.status(404).json({ message: "The post with the specified ID does not exist." })
+    }
+  })
 });
 
 router.get('/:id/comments', (req, res) => {
@@ -31,6 +38,14 @@ router.get('/:id/comments', (req, res) => {
 
 router.post('/', (req, res) => {
 
+  Posts.add(req.body)
+  .then(post => {
+    res.status(201).json(hub);
+  })
+  .catch(error => {
+    console.log(error);
+    res.status(500).json({ error: "There was an error while saving the post to the database" })
+  })
 });
 
 router.post('/:id/comments', (req, res) => {
@@ -38,9 +53,32 @@ router.post('/:id/comments', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
-
+  Posts.remove(req.params.id)
+  .then(count => {
+    if(count > 0) {
+      res.status(200).json({ message: "Post has been removed"});
+    } else {
+      res.status(404).json({ message: "The post with the specified ID does not exist." });
+    }
+  })
+  .catch(error => {
+    console.log(error);
+    res.status(500).json({ error: "The post could not be removed" });
+  });
 });
 
 router.put('/:id', (req, res) => {
-  
-})
+  const changes = req.body;
+  Posts.update(req.params.id, changes)
+  .then(post => {
+    if(post) {
+      res.status(200).json(post);
+    }else{
+      res.status(404).json ({ message: "The post with the specified ID does not exist." });
+    }
+  })
+  .catch(error => {
+    console.log(error);
+    res.status(500).json({ error: "The post information could not be modified." });
+  });
+});
